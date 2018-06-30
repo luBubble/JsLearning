deaaf6fb1a8f09e73641b7b369c9c8bf6d15b724
lulu <1316527338@qq.com>
2018-06-30 14:22:49 +0800

    2018-6-30 add clock and arrayDemo

---
+++ clock/clock.js
@@ -0,0 +1,50 @@
+
+function getTime()
+{
+	var today=new Date();//当前时间
+	var todayDate=today.getFullYear()+addZero(today.getMonth())+addZero(today.getDay());//当前日期
+	changeImg('date-img',todayDate);
+	var todayTime=addZero(today.getHours())+addZero(today.getMinutes())+addZero(today.getSeconds());//现在的时间
+	changeImg('time-img',todayTime);
+}
+function addZero(num)
+{
+	if(num<10)
+	{
+		return '0'+num;
+	}
+	else
+	{
+		return ''+num;
+	}
+}
+function changeImg(type,str)
+{
+	var imgs=document.getElementsByClassName(type);
+	for(var i=0;i<imgs.length;i++)
+	{
+		imgs[i].src="../img/"+str[i]+".png";
+	}
+}
+window.onload=function()
+{
+setInterval(getTime,1000);//1s后第一次执行，此后每隔1s执行一次
+//函数后不能有(),写成getTime而非getTime(),传参"getTime(str)"
+getTime();
+
+//延迟提示框
+var targetImg=document.getElementById("late-img");
+//var targetImg=document.getElementsByClassName("hh");
+//根据类名获取的是元素数组，绑定鼠标移入移出触发的事件具体到单个元素
+var lateAlert=document.getElementById("late-alert");
+targetImg.onmouseover=function()
+{
+	lateAlert.style.display='block';
+};
+
+targetImg.onmouseout=function()
+{
+	lateAlert.style.display='none';
+};
+
+}
---
+++ js/array.js
@@ -0,0 +1,60 @@
+var numArray=[2,45,12,-7,5.6,-3.4,50,132];
+//1.sort排序，默认升序，numArray被改变
+//console.log(numArray);
+//console.log(numArray.sort());
+//console.log(numArray.sort(function(a,b){return a-b;}));//升序
+
+///*2.slice,返回一个数组，numArray不改变
+// * 1个参数a：返回数组从a索引开始的部分
+// */
+//console.log(numArray.slice(2));
+//console.log(numArray.slice(1,5));//不包括5索引
+//console.log(numArray.slice(1,-6));//负参数处理方式：numArray.length-3
+//console.log(numArray.slice(9,10));
+
+/*3.splice
+ * 两个参数
+ * 一个参数
+ */
+//var tempArray=numArray.splice(2,3);
+//console.log(tempArray);//删除了[ 12, -7, 5.6 ]
+//console.log(numArray);//[ 2, 45, -3.4, 50, 132 ]
+
+//console.log(numArray.splice(3,4,'hhh','eee','emmmm'));//索引3开始的后4个
+//console.log(numArray);
+
+//console.log(numArray.splice(3));//索引3后的所有内容
+//console.log(numArray);
+//清空数组
+//numArray.splice(0);
+
+/*4.forEach
+ * 循环
+ */
+numArray.forEach(function(value,index){
+	console.log(index+":"+value)
+});
+
+
+/*5.map()
+ * 返回一个新数组
+ */
+//var tempArray=numArray.map(function(value,index){
+//	return value*2;
+//});
+//console.log(tempArray);
+
+/*6.filter()
+ * 返回一个新数组
+ */
+//var tempArray=numArray.map(function(value,index){
+//	return index%2===0;
+//});
+//console.log(tempArray);
+
+/*7.every
+ * 每个都满足条件返回true
+ */
+/*8.some
+ * 只要有一个满足条件就返回true
+ */
